6a8526c018b2041123c0e774f348b15f060c4834
Dan Friedman <danfriedman9@gmail.com>
2016-03-02 09:51:17 -0500

    finish project

--- js/app.js
+++ js/app.js
@@ -1,20 +1,15 @@
 
+/* TODO
+pull out the response into a function
+add "warmer/colder" style messagings
+*/
+
 $(document).ready(function(){
 	
 	var answer;
 	var guessNumber = 0;
+	var lastGuess = null;
 	newGame();
-
-	/*--- Display information modal box ---*/
-	$(".what").click(function(){
-  	$(".overlay").fadeIn(1000);
-
-	});
-
-	/*--- Hide information modal box ---*/
-	$("a.close").click(function(){
-		$(".overlay").fadeOut(1000);
-	});
 	
 	$('.new').on('click', function() {
 		newGame();
@@ -22,40 +17,49 @@ $(document).ready(function(){
 
 	function newGame() {
 		answer = Math.floor(Math.random() * 100+ 1);
-		guessNumber = -1;
-		guessCounter;
-		$('#feedback').html("Let's play. Make your Guess!");
-		$('.text').val('');
-		$('#guessList').empty();
-	}
-
-	function guessCounter() {
-		guessNumber++;
-		$('#count').html(guessNumber);
-	}
-
-	function listGuesses(guess) {
-		$('#guessList').append("<li>"+guess+"</li>");
-	}
-
-	function validateInput(guess) {
-		guess = parseInt(guess, 10);
-		console.log(guess);
-		if (guess%1===0 && guess<101 && guess>0) {
-			return true;
-			console.log("true");
-		} else {
-			$('#feedback').html("Please enter an integer between 1 and 100");
-		}
+		console.log(answer);
+		$('#count').html("0");
+		guessNumber = 0;
+		lastGuess = null;
+		$('#feedback').html("Let's play. Make your Guess!"); // set initial message
+		$('.text').val(''); // Reset guess placeholder
+		$('#guessList').empty(); // Empty the list of guesses
 	}
 
 	$('#guessButton').on('click', function(event) {
 		event.preventDefault();
 		var guess = $('.text').val();
+
 		if (validateInput(guess)) {
-			guessCounter();
+			guessNumber++;
+			$('#count').html(guessNumber);
+
 			listGuesses(guess);
 
+			guessResponse(guess, lastGuess);
+		}
+		lastGuess = guess;
+	}); 
+
+	function listGuesses(guess) {
+		$('#guessList').append("<li>"+guess+"</li>");
+	} 	
+
+	function validateInput(guess) {
+		guess = parseInt(guess, 10);
+		if (guess%1===0 && guess<101 && guess>0) {
+			return true;
+		}
+		$('#feedback').html("Please enter an integer between 1 and 100");
+		return false;
+	}
+
+	function guessResponse(guess, lastGuess) {
+		var currentDistance = Math.abs(guess-answer);
+		var lastDistance = Math.abs(lastGuess-answer);
+
+		// For first guess
+		if (lastGuess == null) {
 			if (guess<answer) {
 				$('#feedback').html("Too low");
 			} else if (guess>answer) {
@@ -63,8 +67,32 @@ $(document).ready(function(){
 			} else {
 				$('#feedback').html("You got it!");
 			}
+			return;
 		}
-	});  	
+
+		// For guesses > 1 
+		if (currentDistance<5) {
+			$('#feedback').html("So close!");
+		} else if (currentDistance < lastDistance && guess < answer) {
+			$('#feedback').html("Getting closer but too low");
+		} else if (currentDistance < lastDistance && guess > answer) {
+			$('#feedback').html("Getting closer but too high");
+		} else if (lastDistance > currentDistance) {
+			$('#feedback').html("Colder");
+		} else {
+			$('#feedback').html("You got it!");
+		}
+	}
+
+	/*--- Display information modal box ---*/
+	$(".what").click(function(){
+  	$(".overlay").fadeIn(1000);
+	});
+
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
 });
 
 
